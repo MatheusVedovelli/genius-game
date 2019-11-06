b3979bafd7d8a691527f0b21f345cd9ab15f73eb
Matheus Vedovelli <matheusvedovelli@hotmail.com>
2019-11-06 01:58:34 -0300

    Base estrutural do jogo

--- js/script.js
+++ js/script.js
@@ -1,44 +1,42 @@
-$(document).ready(function(){
-    $("#btn1").addClass("padrao-1");
-    $("#btn1").width($(document).width()/4);
-    $("#btn1").height(($(document).height()/5)*2);
-    $("#btn2").addClass("padrao-2");
-    $("#btn2").width($(document).width()/4);
-    $("#btn2").height(($(document).height()/5)*2);
-    $("#btn3").addClass("padrao-3");
-    $("#btn3").width($(document).width()/4);
-    $("#btn3").height(($(document).height()/5)*2);
-    $("#btn4").addClass("padrao-4");
-    $("#btn4").width($(document).width()/4);
-    $("#btn4").height(($(document).height()/5)*2);
+let colors = [["rgb(0, 128, 0)", "rgb(128, 0, 0)", "rgb(128, 128, 0)", "rgb(0, 0, 128)"], // cores de cada botão apagado
+              ["rgb(0, 255, 0)", "rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 0, 255)"]]; // cores de cada botão aceso
 
-    $("#btn1").hover(function(){
-        $("#btn1").removeClass("padrao-1");
-        $("#btn1").addClass("padrao-1-clicked");
-    }, function(){
-        $("#btn1").removeClass("padrao-1-clicked");
-        $("#btn1").addClass("padrao-1");
-    })
-    $("#btn2").hover(function(){
-        $("#btn2").removeClass("padrao-2");
-        $("#btn2").addClass("padrao-2-clicked");
-    }, function(){
-        $("#btn2").removeClass("padrao-2-clicked");
-        $("#btn2").addClass("padrao-2");
-    })
-    $("#btn3").hover(function(){
-        $("#btn3").removeClass("padrao-3");
-        $("#btn3").addClass("padrao-3-clicked");
-    }, function(){
-        $("#btn3").removeClass("padrao-3-clicked");
-        $("#btn3").addClass("padrao-3");
-    })
-    $("#btn4").hover(function(){
-        $("#btn4").removeClass("padrao-4");
-        $("#btn4").addClass("padrao-4-clicked");
-    }, function(){
-        $("#btn4").removeClass("padrao-4-clicked");
-        $("#btn4").addClass("padrao-4");
-    })
-    
+function sleep(ms) { // pausa a execução da função atual (precisa ser async) pelo tempo estipulado em milisegundos
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function setLight(num, state) // seta a luz do botão (0 = apagado | 1 = aceso)
+{
+    if((num < 1 || num > 4) || (state < 0 || state > 1)) return; // só pra garantir que os valores são válidos
+
+    $("#btn" + num).css("color", colors[state][num-1]);
+    $("#btn" + num).css("background-color", colors[state][num-1]);
+}
+
+let currentButton = 0;
+async function flashButton(num) // função que vai gerenciar o botão pressionado pelo usuário (piscar, emitir som e verificar ordem)
+{
+    if(currentButton != 0) return; // permitir apertar o proximo botão apenas quando o ultimo tiver terminado
+
+    currentButton = num;
+    setLight(num, 1);
+
+    // inserir som do botão aqui
+
+    await sleep(1000);
+    setLight(num, 0);
+    currentButton = 0;
+}
+
+$(document).ready(function(){
+    for(let i = 1; i <= 4; i++)
+    {
+        setLight(i, 0);
+        $("#btn" + i).width($(document).width()/4);
+        $("#btn" + i).height(($(document).height()/5)*2);
+
+        $("#btn" + i).click(function(){
+            flashButton(i);
+        });
+    }
 })
